3e18f28c89f6ef42af8c4e8d4c6b9ff701f48d7f
Penina <wpenina@gmail.com>
2026-02-28 19:15:58 +0300

    added signup and sigin logic

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import { useEffect, useState } from 'react';
+import { getAuth } from 'firebase/auth';
 
 function Profile() {
+    const [user, setUser] = useState(null);
+    const auth = getAuth();
+
+    useEffect(() => {
+        setUser(auth.currentUser)
+    }, []);
+
     return (
         <div>
             <h1>Profile</h1>
+
+            {user ? <p>Welcome, <strong>{user.displayName}</strong></p> : <p>Not logged in</p>}
         </div>
-    )
+    );
 }
 
-export default Profile
+export default Profile;
--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,7 +1,10 @@
 import { useState } from 'react'
+import { toast } from 'react-toastify'
 import { Link, useNavigate } from 'react-router-dom'
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth'
 import ArrowRightIcon from '../assets/svg/keyboardArrowRightIcon.svg?react'
-import VisibilityIcon from '../assets/svg/visibilityIcon.svg?react'
+import VisibilityIcon from '../assets/svg/visibilityIcon.svg'
+import firebase from 'firebase/compat/app'
 
 
 function SignIn() {
@@ -16,13 +19,87 @@ function SignIn() {
 
     const navigate = useNavigate()
 
+    const onChange = (e) => {
+        setFormData((prevState) => ({
+            ...prevState,
+            [e.target.id]: e.target.value
+        }))
+    }
+
+    const onSubmit = async (e) => {
+        e.preventDefault()
+
+        try {
+            const auth = getAuth()
+            const userCredential = await signInWithEmailAndPassword(auth, email, password)
+
+            if (userCredential.user) {
+                navigate('/')
+            }
+        } catch (error) {
+            toast.error('Bad User Credentials');
+        }
+
+
+    }
+
     return (
         <>
             <div className="pageContainer">
                 <header>
                     <p className="pageHeader"> Welcome Back!</p>
                 </header>
-            </div>
+                <main>
+                    <form onSubmit={onSubmit}>
+                        <input
+                            type="email"
+                            className="emailInput"
+                            placeholder="Email"
+                            id="email" value={email}
+                            onChange={onChange}
+                        />
+                        <div className="passwordInputDiv">
+                            <input
+                                type={showPassword ? 'text' : 'password'}
+                                className="passwordInput"
+                                placeholder="Password"
+                                id="password"
+                                value={password}
+                                onChange={onChange}
+                            />
+                            <img
+                                src={VisibilityIcon}
+                                alt="Show Password"
+                                className="showPassword"
+                                onClick={() => setShowPassword((prevState) => !prevState)}
+                            />
+                        </div>
+                        <Link
+                            to='/forgot-password'
+                            className="forgotPasswordLink"
+                        >
+                            Forgot Password
+                        </Link>
+                        <div className="signInBar">
+                            <p className="signInText">
+                                Sign In
+                            </p>
+                            <button className="signInButton">
+                                <ArrowRightIcon fill="#ffffff" width="34px" height="34px" />
+                            </button>
+                        </div>
+                    </form>
+
+                    {/*Google OAuth  */}
+
+                    <Link
+                        to="/sign-up"
+                        className="registerLink"
+                    >
+                        Sign Up Instead
+                    </Link>
+                </main>
+            </div >
         </>
     )
 }
